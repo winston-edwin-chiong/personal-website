25d8ecf5776fb0436187b86ca661f5e56f6af3d4
Winston Chiong <winstonchiong@berkeley.edu>
2023-12-26 01:41:18 -0800

    remove button borders and background on nav and footer, edit projects and coursework

--- src/app/page.js
+++ src/app/page.js
@@ -9,12 +9,12 @@ export default function Home() {
         <div className="text-4xl tracking-widest uppercase text-center [word-spacing:10px] px-10 sm:text-5xl">
           <AnimatedWinston />
         </div>
-        <div className="flex flex-col items-center mt-6 sm:flex-row sm:space-x-8 children:pb-6">
-          <HomeNavButton href={"/about"} label={"About"} />
-          <HomeNavButton href={"/experience"} label={"Experience"} />
-          <HomeNavButton href={"/blog"} label={"Blog"} />
-          <HomeNavButton href={"/song"} label={"Song"} />
-          <HomeNavButton href={"/cat"} label={"Cat"} />
+        <div className="flex flex-col items-center mt-8 sm:flex-row children:mb-10">
+          <HomeNavButton href={"/about"} label={"ABOUT"} />
+          <HomeNavButton href={"/experience"} label={"EXPERIENCE"} />
+          <HomeNavButton href={"/blog"} label={"BLOG"} />
+          <HomeNavButton href={"/song"} label={"SONG"} />
+          <HomeNavButton href={"/cat"} label={"CAT"} />
         </div>
       </div>
     </>
@@ -23,10 +23,10 @@ export default function Home() {
 
 function HomeNavButton({ href, label }) {
   return (
-    <Link href={href}>
-      <button className="btn btn-primary tracking-widest hover:bg-secondary-focus/[.1] hover:transition-all hover:ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+    <Link href={href} className="my-1 mx-8">
+      <div className="p-1 font-semibold text-sm tracking-widest hover:transition-all hover:ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
         {label}
-      </button>
+      </div>
     </Link>
   );
 }
